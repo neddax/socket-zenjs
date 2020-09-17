1133b0e6bc559a577b5fd3f263f2e4ee810ed31f
CarterScadden <carter@scaddenfamily.com>
2020-09-16 18:09:50 -0600

    changed the build from imports to requires

--- src/LowLevel/index.ts
+++ src/LowLevel/index.ts
@@ -17,10 +17,6 @@ export default class LowLevel {
     return this;
   }
 
-  get getConnections() {
-    return this._connections;
-  }
-
   _removeConnection(connection: Socket) {
     this._connections = this._connections.filter((c) => c !== connection);
   }
